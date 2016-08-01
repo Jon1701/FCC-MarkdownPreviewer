51c5f58a7c00d104906aa61816714f99ac6ea39f
Jon Balon <development@jonbalon.com>
2016-07-31 21:41:00 -0400

    Added skeleton of TextArea component, added styling

--- source/javascript/MarkdownPreviewer/TextArea.jsx
+++ source/javascript/MarkdownPreviewer/TextArea.jsx
@@ -3,9 +3,18 @@ import React from 'react';
 class TextArea extends React.Component {
   render() {
     return (
-      <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 text-center">
-        TextArea
+      <div className="section-textarea col-xs-12 col-sm-6 col-md-6 col-lg-6 text-center">
+
+        <div className="header">
+          <h2 className="text-center">Markdown</h2>
+        </div>
+        <div className="content">
+          <textarea>
+          </textarea>
+        </div>
       </div>
+
+
     )
   }
 }
