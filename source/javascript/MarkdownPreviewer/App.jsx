609e31bbd427ad10723891f882c66885ac1b2f66
Jon Balon <development@jonbalon.com>
2016-07-31 21:15:28 -0400

    Added TextArea and PreviewArea components

--- source/javascript/MarkdownPreviewer/App.jsx
+++ source/javascript/MarkdownPreviewer/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import TextArea from './TextArea.jsx';
+import PreviewArea from './PreviewArea.jsx';
+
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello World
+      <div className="container">
+
+        <div className="container text-center">
+          <h1>FreeCodeCamp Markdown Previewer</h1>
+        </div>
+        <div className="container">
+          <TextArea/>
+          <PreviewArea/>
+        </div>
       </div>
     )
   }
---
+++ source/javascript/MarkdownPreviewer/PreviewArea.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PreviewArea extends React.Component {
+  render() {
+    return (
+      <div>
+        PreviewArea
+      </div>
+    )
+  }
+}
+
+export default PreviewArea;
