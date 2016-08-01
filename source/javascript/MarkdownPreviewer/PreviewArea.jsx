e50842b2453f2bf7a8fc8b618cbcd1aab55f7870
Jon Balon <development@jonbalon.com>
2016-08-01 01:25:25 -0400

    Implemented markdown conversion

--- source/javascript/MarkdownPreviewer/PreviewArea.jsx
+++ source/javascript/MarkdownPreviewer/PreviewArea.jsx
@@ -1,28 +1,28 @@
+// React.
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Markdown.
 var marked = require('marked');
 
 class PreviewArea extends React.Component {
 
-  createMarkup(value) {
-
-    var x = marked(value, {sanitize: true});
-
-    return {
-      __html: x
-    }
+  // When component triggers an update, render the markdown as HTML markup.
+  componentDidUpdate() {
+    this.refs.preview.innerHTML = marked(this.props.markdown);
   }
 
   // Component render.
   render() {
+
     return (
-      <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 text-center">
+      <div id="section-preview" className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
 
         <div className="header">
           <h2 className="text-center">Preview</h2>
         </div>
-        <div
-          className="content"
-          dangerouslySetInnerHtml={this.createMarkup(this.props.markdown)}>
+
+        <div className="content" ref="preview">
         </div>
 
       </div>
