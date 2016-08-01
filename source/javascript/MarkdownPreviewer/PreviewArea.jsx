7f0d74f1de9a5f1e3ba9ebb9a0b3b835ce3eb9ae
Jon Balon <development@jonbalon.com>
2016-07-31 21:16:59 -0400

    Added responsive classes

--- source/javascript/MarkdownPreviewer/PreviewArea.jsx
+++ source/javascript/MarkdownPreviewer/PreviewArea.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class PreviewArea extends React.Component {
   render() {
     return (
-      <div>
+      <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 text-center">
         PreviewArea
       </div>
     )
