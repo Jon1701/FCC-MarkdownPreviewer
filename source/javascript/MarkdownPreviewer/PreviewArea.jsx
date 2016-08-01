0afca16e3d6e2fad2fcb5481f54460987f0247a0
Jon Balon <development@jonbalon.com>
2016-07-31 23:46:15 -0400

    Added HTML rendering of markdown

--- source/javascript/MarkdownPreviewer/PreviewArea.jsx
+++ source/javascript/MarkdownPreviewer/PreviewArea.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+var marked = require('marked');
 
 class PreviewArea extends React.Component {
+
+  createMarkup(value) {
+
+    var x = marked(value, {sanitize: true});
+
+    return {
+      __html: x
+    }
+  }
+
+  // Component render.
   render() {
     return (
       <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 text-center">
 
-      <div className="header">
-        <h2 className="text-center">Preview</h2>
-      </div>
-      <div className="content">
-        {this.props.markdown}
-      </div>
+        <div className="header">
+          <h2 className="text-center">Preview</h2>
+        </div>
+        <div
+          className="content"
+          dangerouslySetInnerHtml={this.createMarkup(this.props.markdown)}>
+        </div>
 
       </div>
     )
