b9c1a9f899f76a476d38e03f6dc84be5e2985bc9
Jon Balon <development@jonbalon.com>
2016-07-31 22:16:54 -0400

    Added skeleton

--- source/javascript/MarkdownPreviewer/PreviewArea.jsx
+++ source/javascript/MarkdownPreviewer/PreviewArea.jsx
@@ -4,7 +4,14 @@ class PreviewArea extends React.Component {
   render() {
     return (
       <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 text-center">
-        PreviewArea
+
+      <div className="header">
+        <h2 className="text-center">Preview</h2>
+      </div>
+      <div className="content">
+        {this.props.markdown}
+      </div>
+
       </div>
     )
   }
